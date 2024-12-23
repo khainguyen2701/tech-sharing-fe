96add19ea6879c6812ba00d4e2861b40e49d7a69
Nguyễn Văn Khai <khai.nguyen@kyanon.digital>
2024-12-23 10:50:26 +0700

    feat: install axios and react-query

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from 'react-query/devtools'
+
 function App() {
+  const queryClient = new QueryClient()
   return (
-    <div className='mx-auto flex max-w-sm items-center gap-x-4 rounded-xl bg-white p-6 shadow-lg'>
+    <QueryClientProvider client={queryClient}>
+      <div className='mx-auto flex max-w-sm items-center gap-x-4 rounded-xl bg-white p-6 shadow-lg'>
       <div className='shrink-0'>
         <img className='size-12' src='/img/logo.svg' alt='ChitChat Logo' />
       </div>
@@ -9,6 +14,9 @@ function App() {
         <p className='text-slate-500'>You have a new message!</p>
       </div>
     </div>
+    <ReactQueryDevtools initialIsOpen={true}  />
+    </QueryClientProvider>
+    
   );
 }
 
